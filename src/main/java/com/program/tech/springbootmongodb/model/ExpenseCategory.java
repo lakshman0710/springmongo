a477a8227cf7d6ae8a7db22101a0ee5e84360f4b
lakshman <lakshmanbala14@gmail.com>
2024-01-28 18:28:28 +0530

    add springmongo master

---
+++ src/main/java/com/program/tech/springbootmongodb/model/ExpenseCategory.java
@@ -0,0 +1,5 @@
+package com.program.tech.springbootmongodb.model;
+
+public enum ExpenseCategory {
+	ENTERTAINMENT, GROCERIES, RESTAURANT, UTILITIES, MISC
+}
---
+++ src/main/java/com/program/tech/springbootmongodb/sevice/ExpenseService.java
@@ -0,0 +1,49 @@
+package com.program.tech.springbootmongodb.sevice;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.program.tech.springbootmongodb.model.Expense;
+import com.program.tech.springbootmongodb.repository.ExpenseRepository;
+
+@Service
+public class ExpenseService {
+	
+	private final ExpenseRepository expenseRepository;
+	
+	public ExpenseService(ExpenseRepository expenseRepository) {
+		this.expenseRepository = expenseRepository;
+	}
+	
+	
+	public void addExpense(Expense expense){
+		expenseRepository.insert(expense);
+	}
+	
+	public void updateExpense(Expense expense){
+		Expense savedExpense = expenseRepository.findById(expense.getId())
+				.orElseThrow(() -> new RuntimeException(
+						String.format("cannot Find Expense by ID %s", expense.getId())));
+		
+		savedExpense.setExpenseName(expense.getExpenseName());
+        savedExpense.setExpenseCategory(expense.getExpenseCategory());
+        savedExpense.setExpenseAmount(expense.getExpenseAmount());
+ 
+        expenseRepository.save(expense);
+	}
+	
+	public List<Expense> getAllExpenses(){
+		return expenseRepository.findAll();
+	}
+	
+	
+	public Expense getExpenseByName(String name) {
+        return expenseRepository.findByName(name)
+                .orElseThrow(() -> new RuntimeException(String.format("Cannot Find Expense by Name - %s", name)));
+    }
+	
+	public void deleteExpense(String id) {
+        expenseRepository.deleteById(id);
+    }
+}
